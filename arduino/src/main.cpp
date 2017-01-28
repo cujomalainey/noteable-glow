3510132a7736c2c93c22a3b9490a8aeb2fb22ef9
Curtis Malainey <curtis@malainey.com>
2017-01-28 15:29:38 -0700

    Generalized LED input created

--- arduino/src/main.cpp
+++ arduino/src/main.cpp
@@ -1,24 +1,39 @@
 #include <Arduino.h>
+#include <Adafruit_NeoPixel.h>
 #include "HSLtoRGB.h"
 
+#define LED_PIN   0
 #define BAUD_RATE 9600
+#define SATURATION 100
+#define NUMPIXELS 60
+
+#define INTENSITY_TO_HUE (360.0/100)
+
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, LED_PIN, NEO_GRB + NEO_KHZ800);
+
+void set_pixel(uint8_t i, uint8_t intensity);
 
 void setup()
 {
 	Serial.begin(BAUD_RATE);
-	RGB_point rgb = HSL_2_RGB( {200, 100, 75} );
-	delay(5000);
-	Serial.print("R: ");
-	Serial.println(rgb.r);
-	Serial.print("G: ");
-	Serial.println(rgb.g);
-	Serial.print("B: ");
-	Serial.println(rgb.b);
+    pixels.begin();
 }
 
 void loop()
 {
+        for (uint8_t i = 0; i < NUMPIXELS; i++)
+        {
+            set_pixel(i, i);
+        }
+        pixels.show();
+        delay(50);
+}
 
+void set_pixel(uint8_t i, uint8_t intensity)
+{
+    uint16_t v1 = (uint16_t)(((double)intensity) * INTENSITY_TO_HUE);
+    RGB_point rgb = HSL_2_RGB( {v1, SATURATION, intensity} );
+    pixels.setPixelColor(i,pixels.Color(rgb.r, rgb.g, rgb.b));
 }
 
     //         pb_istream_t stream = pb_istream_from_buffer(zrr.getData(), zrr.getDataLength());
