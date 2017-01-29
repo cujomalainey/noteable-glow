a92ca27e87a67e7644873e1c0cfa62b67be8b8c0
Curtis Malainey <curtis@malainey.com>
2017-01-28 18:19:54 -0700

    init proto

--- raspberrypi/c/main.cpp
+++ raspberrypi/c/main.cpp
@@ -4,10 +4,16 @@
 #include <pthread.h>
 #include <unistd.h>
 #include "serial.h"
+#include "proto_packer.h"
+
+#define BUFFER_SIZE 256
+
+static uint8_t buffer[BUFFER_SIZE];
 
 int main()
 {
 	setup_serial();
+	configure_buffer(buffer, BUFFER_SIZE);
 	uint8_t data = 1;
 	send_bytes(&data, 1);
 	sleep(1);
